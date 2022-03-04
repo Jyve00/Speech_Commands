d96b66a7104e531a3a2a68decbe4a3ea56a5a82a
Stephen William <swilliam.productions@gmail.com>
2022-03-04 17:22:56 -0500

    greedydecoder

--- my_functions.py
+++ my_functions.py
@@ -231,3 +231,29 @@ class DeepSpeech(nn.Module):
 
 
 
+def GreedyDecoder(output, labels, label_lengths, blank_label=28, collapse_repeated=True):
+	"""outputs the most probable character at each time step.
+
+	Args:
+		output (_type_): _description_
+		labels (_type_): _description_
+		label_lengths (_type_): _description_
+		blank_label (int, optional): _description_. Defaults to 28.
+		collapse_repeated (bool, optional): _description_. Defaults to True.
+
+	Returns:
+		_type_: _description_
+	"""
+	arg_maxes = torch.argmax(output, dim=2)
+	decodes = []
+	targets = []
+	for i, args in enumerate(arg_maxes):
+		decode = []
+		targets.append(text_transform.int_to_text(labels[i][:label_lengths[i]].tolist()))
+		for j, index in enumerate(args):
+			if index != blank_label:
+				if collapse_repeated and j != 0 and index == args[j -1]:
+					continue
+				decode.append(index.item())
+		decodes.append(text_transform.int_to_text(decode))
+	return decodes, targets
