5b79a78c88bc9db60ffe9006c4da148baca96081
Stephen William <swilliam.productions@gmail.com>
2022-03-04 13:55:33 -0500

    removed mel spec

--- my_functions.py
+++ my_functions.py
@@ -1,25 +1,9 @@
 import torch 
 import torchaudio 
-torch.nn as nn
+import torch.nn as nn
 import numpy as np 
-import numpy as np as np
+import pandas as pd 
 
-
-# class for melspec 
-
-class LogMelSpec(nn.Module):
-
-    def __init__(self, sample_rate=8000, n_mels=128, win_length=160, hop_length=80):
-        super(LogMelSpec, self).__init__()
-        self.transform = torchaudio.transforms.MelSpectrogram(
-            sample_rate=sample_rate, win_length=win_length,
-            n_mels-n_mels, hop_length=hop_length
-        )
-    
-    def forward(self. x):
-        x = self.transform(x)
-        x = np.log(x + 1e-14)
-        return x
 
 
 class TextTransform:
