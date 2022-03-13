67bfccd0a0a54a092a54a6fa6e4859c22a66450c
Stephen William <swilliam.productions@gmail.com>
2022-03-13 04:37:33 -0400

    sure

---
+++ asssembly_api.py
@@ -0,0 +1,10 @@
+import requests 
+import pprint   
+import json 
+import time 
+
+upload_endpoint = 'https://api.assemblyai.com/v2/upload'
+transcript_endpoint = 'https://api.assemblyai.com/v2/transcript'
+
+/Users/stephen/code/projects/Speech_Recognition/Data
+
